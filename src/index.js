63cab85c833f29095b1acd9706701dbaf7d10143
uttne <uttne.mkr@gmail.com>
2022-04-04 00:55:32 +0900

    複数ページの出力に対応

--- src/index.js
+++ src/index.js
@@ -1,36 +1,74 @@
-const scoreButtonId = "score-github-button-id";
+const scoreViewButtonId = "score-github-view-button-id";
+const scoreNextButtonId = "score-github-next-button-id";
+const scorePrevButtonId = "score-github-prev-button-id";
 
 const scoreImageDivId = "score-github-image-container-id";
 
 function setButtons() {
     const buttonDiv = document.getElementsByClassName("BtnGroup")[1];
     if (!buttonDiv) return false;
-    const button = document.getElementById(scoreButtonId);
-    if (button) return true;
 
-    const a = document.createElement("a");
-    a.id = scoreButtonId;
-    a.innerText = "Xml";
-    a.className = "btn-sm btn BtnGroup-item";
-    a.href = "javascript:void(0);";
-    a.onclick = () => {
-        const text = a.innerText;
-        if (text !== "Score") {
-            scoreM.hideScore();
-            a.innerText = "Score";
-        } else {
-            scoreM.showScore();
-            a.innerText = "Xml";
-        }
-    };
+    const viewButton = document.getElementById(scoreViewButtonId);
 
-    buttonDiv.insertBefore(a, buttonDiv.children[0]);
+    if (!viewButton) {
+        const a = document.createElement("a");
+        a.id = scoreViewButtonId;
+        a.innerText = "Xml";
+        a.className = "btn-sm btn BtnGroup-item";
+        a.href = "javascript:void(0);";
+        a.onclick = () => {
+            const text = a.innerText;
+            if (text !== "Score") {
+                scoreM.hideScore();
+                a.innerText = "Score";
+            } else {
+                scoreM.showScore();
+                a.innerText = "Xml";
+            }
+        };
+
+        buttonDiv.insertBefore(a, buttonDiv.children[0]);
+    }
+
+    const nextButton = document.getElementById(scoreNextButtonId);
+
+    if (!nextButton) {
+        const a = document.createElement("a");
+        a.id = scoreNextButtonId;
+        a.innerText = "Next";
+        a.className = "btn-sm btn BtnGroup-item";
+        a.href = "javascript:void(0);";
+        a.onclick = () => {
+            scoreM.next();
+        };
+
+        buttonDiv.insertBefore(a, buttonDiv.children[0]);
+    }
+
+    const prevButton = document.getElementById(scorePrevButtonId);
+
+    if (!prevButton) {
+        const a = document.createElement("a");
+        a.id = scorePrevButtonId;
+        a.innerText = "Prev";
+        a.className = "btn-sm btn BtnGroup-item";
+        a.href = "javascript:void(0);";
+        a.onclick = () => {
+            scoreM.prev();
+        };
+
+        buttonDiv.insertBefore(a, buttonDiv.children[0]);
+    }
+    return true;
 }
 
 class scoreManager {
     constructor() {
         this.svg = "";
         this.rawUrl = "";
+        this.tk = undefined;
+        this.pageCount = 0;
+        this.currentPage = 1;
 
         const imageContainerDiv = document.createElement("div");
         imageContainerDiv.id = scoreImageDivId;
@@ -46,11 +84,40 @@ class scoreManager {
             const xml = await fetch(rawUrl).then((r) => r.text());
 
             var tk = new verovio.toolkit();
+            this.tk = tk;
             const svg = tk.renderData(xml, {});
+            this.pageCount = tk.getPageCount();
 
             this.svg = svg;
             this.rawUrl = rawUrl;
         }
+
+        this.imageContainerDiv.innerHTML = this.svg;
+    }
+
+    prev() {
+        if (!this.tk) {
+            return;
+        }
+        if (this.currentPage <= 1) {
+            return;
+        }
+        this.currentPage--;
+        const svg = this.tk.renderToSVG(this.currentPage);
+        this.svg = svg;
+
+        this.imageContainerDiv.innerHTML = this.svg;
+    }
+    next() {
+        if (!this.tk) {
+            return;
+        }
+        if (this.pageCount <= this.currentPage) {
+            return;
+        }
+        this.currentPage++;
+        const svg = this.tk.renderToSVG(this.currentPage);
+        this.svg = svg;
 
         this.imageContainerDiv.innerHTML = this.svg;
     }
