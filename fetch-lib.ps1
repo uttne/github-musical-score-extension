ed5b4eddb78540c4f01deae7b59b79acb8b1d269
uttne <uttne.mkr@gmail.com>
2022-04-12 23:08:31 +0900

    fix ライブラリのバージョンを修正

--- fetch-lib.ps1
+++ fetch-lib.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest "https://www.verovio.org/javascript/2.7.1/verovio-toolkit.js" -OutFile "$PSScriptRoot/src/verovio-toolkit.js"
+Invoke-WebRequest "https://www.verovio.org/javascript/3.9.0/verovio-toolkit.js" -OutFile "$PSScriptRoot/src/verovio-toolkit.js"
